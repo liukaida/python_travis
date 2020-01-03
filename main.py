3b2ba32560f3cf953d62a160bce91e7981344eec
liukaida <15966987+liukaida@users.noreply.github.com>
2020-01-03 15:04:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 cur_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"\n"
 
 
-filename = 'test_text.txt'
+filename = './www/test_text.txt'
 with open(filename, 'a') as file_object:
     file_object.write(cur_time)
     print(cur_time)
